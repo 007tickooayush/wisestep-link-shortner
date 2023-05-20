ae5b967b99323249d00b1ab26c84eb987586e703
Ayush Tickoo <48850318+007tickooayush@users.noreply.github.com>
2023-05-20 13:53:17 +0530

    added direct browser link Redirection for a valid url and other miscellaneous optimizations

--- back-end/link-shortner-spring/src/main/java/com/wisestep/linkshortnerspring/controller/LinkController.java
+++ back-end/link-shortner-spring/src/main/java/com/wisestep/linkshortnerspring/controller/LinkController.java
@@ -7,13 +7,12 @@ import com.wisestep.linkshortnerspring.util.LinkShortProcessor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.result.view.RedirectView;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.security.SecureRandom;
-import java.util.Date;
-import java.util.Random;
-import java.util.stream.Collectors;
+import java.net.URI;
+import java.time.Duration;
 
 @RestController
 @RequestMapping("/link-short/")
@@ -27,35 +26,52 @@ public class LinkController {
         this.linkService = linkService;
     }
 
+    @GetMapping("")
+    public ResponseEntity<Flux<LinkDto>> getDefault() {
+
+        return ResponseEntity.status(HttpStatus.OK).body(linkService.getAllLinks());
+    }
+
+    @GetMapping("/{shortLink}")
+    public ResponseEntity<Void> getShortenedComplete(@PathVariable("shortLink") String shortUrlToken) {
+
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .location(URI.create("https://"+linkService.getLinkShort(shortUrlToken).map(LinkDto::getRawLink)
+                        .block(Duration.ofSeconds(10))))
+                .build();
+    }
+
+//    @GetMapping("/{shortLink}")
+//    public ResponseEntity<Mono<LinkDto>> getShortenedComplete(@PathVariable("shortLink") String shortUrlToken){
+//        return ResponseEntity.status(HttpStatus.FOUND).body(linkService.getLinkShort(shortUrlToken));
+//    }
+
+
+    @PostMapping("/insert")
+    public ResponseEntity<Mono<LinkDto>> createLink(@RequestBody OnlyLinkDto rawLinkObj) {
+
+        // save the link generated into the database and return the response
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(linkService.insertShortLink(Mono.just(new LinkShortProcessor().generateShortLink(rawLinkObj))));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Mono<LinkDto>> updateLink(@RequestBody OnlyLinkDto rawOnlyLinkObj) {
+
+        return ResponseEntity.status(HttpStatus.OK).body(linkService.updateShortLink(rawOnlyLinkObj));
+    }
+
+    //    Miscellaneous calls for debugging
     @GetMapping("/raw-link")
-    public ResponseEntity<Mono<LinkDto>> getByRawLink(@RequestParam("link") String rawLinkObj){
+    public ResponseEntity<Mono<LinkDto>> getByRawLink(@RequestParam("link") String rawLinkObj) {
 
         return ResponseEntity.status(HttpStatus.OK).body(linkService.getLinkRaw(rawLinkObj));
     }
 
     @GetMapping("/short-link")
-    public ResponseEntity<Mono<LinkDto>> getByShortLink(@RequestParam("link") String shortLinkObj){
+    public ResponseEntity<Mono<LinkDto>> getByShortLink(@RequestParam("link") String shortLinkObj) {
 
         return ResponseEntity.status(HttpStatus.OK).body(linkService.getLinkShort(shortLinkObj));
     }
 
-    @GetMapping("")
-    public ResponseEntity<Flux<LinkDto>> getDefault(){
-
-        return ResponseEntity.status(HttpStatus.OK).body(linkService.getAllLinks());
-    }
-
-    @PostMapping("/insert")
-    public ResponseEntity<Mono<LinkDto>> createLink(@RequestBody OnlyLinkDto rawLinkObj){
-
-        // save the link generated into the database and return the response
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(linkService.insertShortLink(Mono.just( new LinkShortProcessor().generateShortLink(rawLinkObj))));
-    }
-
-    @PutMapping("/update")
-    public ResponseEntity<Mono<LinkDto>> updateLink(@RequestBody OnlyLinkDto rawOnlyLinkObj){
-
-        return ResponseEntity.status(HttpStatus.OK).body(linkService.updateShortLink(rawOnlyLinkObj));
-    }
 }
